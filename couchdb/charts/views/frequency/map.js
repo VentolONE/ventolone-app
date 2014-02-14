57e553554059d11e85aad22322eb002940e64672
Carlo Colombo <carlo.colombo@gmx.com>
2014-02-14 23:50:46 +0100

    updated frequency view with commonjs module

--- couchdb/charts/views/frequency/map.js
+++ couchdb/charts/views/frequency/map.js
@@ -1,19 +1,3 @@
 function(doc) {
-  if (doc.speed != 0) {
-    var intervals = [3600 * 24, 3600 * 6, 3600, 1800, 600, 60]
-    var value = parseInt(doc.speed / 0.5) * 0.5
-    var keys = []
-
-    for (var i in intervals) {
-      keys.push(new Date(parseInt(doc.time / intervals[i]) * intervals[i] * 1000))
-      keys.push(value)
-    }
-    var d = keys[0]
-
-    emit([
-      doc.anemometerId,
-      new Date(d.getUTCFullYear(),d.getMonth(), 1),
-      value
-    ].concat(keys), 1);
-  }
+  emit(require('views/lib/utils').generateKeys(doc.anemometerId, doc), 1)
 }
