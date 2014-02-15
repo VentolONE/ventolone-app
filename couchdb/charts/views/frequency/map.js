4f4bce0954048338e5379880fc419943f8c08c1b
Carlo Colombo <carlo.colombo@gmx.com>
2014-02-15 09:36:04 +0100

    fix view

--- couchdb/charts/views/frequency/map.js
+++ couchdb/charts/views/frequency/map.js
@@ -1,4 +1,12 @@
 function(doc) {
-  var value = parseInt(doc.speed / 0.5) * 0.5
-  emit(require('views/lib/utils').generateKeys(doc.anemometerId, doc).concat(value), 1)
+  if (doc.speed) {
+    var value = parseInt(doc.speed / 0.5) * 0.5,
+      keys = require('views/lib/utils').generateKeys(value, doc),
+      date = keys[1];
+
+    emit([
+      doc.anemometerId,
+      new Date(date.getUTCFullYear(), date.getMonth(), 1),
+    ].concat(keys), 1)
+  }
 }
