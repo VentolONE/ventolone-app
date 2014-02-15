b37fcc509505d6cc89dfca702712b77a067adce4
Carlo Colombo <carlo.colombo@gmx.com>
2014-02-15 10:17:05 +0100

    fixed views (again)

--- couchdb/charts/views/frequency/map.js
+++ couchdb/charts/views/frequency/map.js
@@ -1,12 +1,19 @@
 function(doc) {
   if (doc.speed) {
     var value = parseInt(doc.speed / 0.5) * 0.5,
-      keys = require('views/lib/utils').generateKeys(value, doc),
-      date = keys[1];
+      intervalsKeys = require('views/lib/utils').intervalsKeys(doc),
+      date = intervalsKeys[1],
+      keys = [];
+
+    for(var i in intervalsKeys){
+      keys.push(intervalsKeys[i])
+      keys.push(value)
+    }
 
     emit([
       doc.anemometerId,
       new Date(date.getUTCFullYear(), date.getMonth(), 1),
+      value
     ].concat(keys), 1)
   }
 }
--- couchdb/charts/views/lib/utils.js
+++ couchdb/charts/views/lib/utils.js
@@ -1,14 +1,20 @@
-exports.generateKeys = function generateKeys(firstKey, doc) {
-  var intervals = [3600 * 24, 3600 * 6, 3600, 1800, 600, 60]
-  var keys = []
+function intervalsKeys(doc) {
+  var intervals = [3600 * 24, 3600 * 6, 3600, 1800, 600, 60],
+    keys = [],
+    date;
 
   for (var i in intervals) {
     keys.push(new Date(parseInt(doc.time / intervals[i]) * intervals[i] * 1000))
   }
-  var d = keys[0]
+  date = keys[0]
 
-  return [
-    firstKey,
-    new Date(d.getUTCFullYear(), d.getMonth(), 1)
-  ].concat(keys)
+  keys.unshift(new Date(date.getUTCFullYear(), date.getMonth(), 1))
+  return keys
 }
+
+function generateKeys(firstKey, doc) {
+  return [firstKey].concat(intervalsKeys(doc))
+}
+
+exports.intervalsKeys = intervalsKeys
+exports.generateKeys  = generateKeys
