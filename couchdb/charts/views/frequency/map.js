168968408601df82267fa99ecab7263623641b47
Carlo Colombo <carlo.colombo@gmx.com>
2014-02-15 09:00:55 +0100

    missing values

--- couchdb/charts/views/frequency/map.js
+++ couchdb/charts/views/frequency/map.js
@@ -1,3 +1,4 @@
 function(doc) {
-  emit(require('views/lib/utils').generateKeys(doc.anemometerId, doc), 1)
+  var value = parseInt(doc.speed / 0.5) * 0.5
+  emit(require('views/lib/utils').generateKeys(doc.anemometerId, doc).concat(value), 1)
 }
