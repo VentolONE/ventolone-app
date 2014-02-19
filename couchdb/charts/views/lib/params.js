f4ff0c2615207036f6f046df45bc7abaccdcf441
Carlo Colombo <carlo.colombo@gmx.com>
2014-02-19 23:55:46 +0100

    group levels

--- couchdb/charts/views/lib/params.js
+++ couchdb/charts/views/lib/params.js
@@ -1,1 +1,10 @@
 exports.intervals = [3600 * 24, 3600 * 6, 3600, 1800, 600, 60]
+exports.intervalsGroupLevels = {
+  month: 2,
+  day: 3,
+  six_hours: 4,
+  hour: 5,
+  half_hour: 6,
+  ten_minute: 7,
+  minute: 8
+}
