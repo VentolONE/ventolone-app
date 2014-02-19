5d2c08290f0a6d8a5e28426af0b4103876b3984c
Carlo Colombo <carlo.colombo@gmx.com>
2014-02-19 23:54:37 +0100

    unified count for values

--- couchdb/charts/views/statistics/map.js
+++ couchdb/charts/views/statistics/map.js
@@ -3,20 +3,18 @@ function(doc) {
     speed: {
       max: parseFloat(doc.speed),
       min: parseFloat(doc.speed),
-      sum: parseFloat(doc.speed),
-      count: 1
+      sum: parseFloat(doc.speed)
     },
     time: {
       max: parseFloat(doc.time),
       min: parseFloat(doc.time),
-      sum: parseFloat(doc.time),
-      count: 1
+      sum: parseFloat(doc.time)
     },
     battery: {
       max: parseFloat(doc.battery),
       min: parseFloat(doc.battery),
-      sum: parseFloat(doc.battery),
-      count: 1
-    }
+      sum: parseFloat(doc.battery)
+    },
+    count: 1
   });
 }
--- couchdb/charts/views/statistics/reduce.js
+++ couchdb/charts/views/statistics/reduce.js
@@ -4,17 +4,16 @@ function(keys,values) {
     res.speed.max = Math.max(values[i].speed.max, res.speed.max)
     res.speed.min = Math.min(values[i].speed.min, res.speed.min)
     res.speed.sum = values[i].speed.sum + res.speed.sum
-    res.speed.count+=values[i].speed.count
 
     res.time.max = Math.max(values[i].time.max, res.time.max)
     res.time.min = Math.min(values[i].time.min, res.time.min)
     res.time.sum = values[i].time.sum + res.time.sum
-    res.time.count+=values[i].time.count
 
     res.battery.max = Math.max(values[i].battery.max, res.battery.max)
     res.battery.min = Math.min(values[i].battery.min, res.battery.min)
     res.battery.sum = values[i].battery.sum + res.battery.sum
-    res.battery.count+=values[i].battery.count
+
+    res.count+=values[i].count
   }
   return res
 }
