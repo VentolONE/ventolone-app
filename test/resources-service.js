a0b72f27dad83b2a75e13b669b439011f9b145f2
Carlo Colombo <carlo.colombo@gmx.com>
2014-04-10 19:56:58 +0200

    first test

---
+++ test/resources-service.js
@@ -0,0 +1,8 @@
+describe('Ventolone.resources module', function() {
+
+  beforeEach(module('Ventolone.resources.services'));
+
+  it('asdsdasd', inject(function(anemometerById) {
+    Should(anemometerById).be.a.Function
+  }))
+})
