c404453d380b04442c41fd3b43f8cb8b498c8234
Carlo Colombo <carlo.colombo@gmx.com>
2014-04-10 22:54:58 +0200

    testing

--- test/resources-service.js
+++ test/resources-service.js
@@ -1,8 +1,48 @@
 describe('Ventolone.resources module', function() {
 
-  beforeEach(module('Ventolone.resources.services'));
+  beforeEach(module('Ventolone.resources.services'))
 
-  it('asdsdasd', inject(function(anemometerById) {
-    Should(anemometerById).be.a.Function
+  beforeEach(module(function($provide) {
+    $provide.factory('Anemometer', function($q) {
+      return {
+        get: function(obj) {
+          var deferred = $q.defer()
+          deferred.resolve({
+            _id: 42
+          })
+          return {
+            $promise: deferred.promise
+          }
+        }
+      }
+    })
   }))
+
+  function assertPromise(p) {
+    Should(p['then']).be.a.Function
+    Should(p['catch']).be.a.Function
+    Should(p['finally']).be.a.Function
+  }
+
+  describe('anemometerById', function() {
+    it('should be defined and a function', inject(function(anemometerById) {
+      Should(anemometerById).be.a.Function
+    }))
+
+    it('should return null if called with null as parameter', inject(function(anemometerById) {
+      Should(anemometerById(null)).be.equal(null)
+    }))
+
+    it('should return a promise of a resource', inject(function(anemometerById) {
+      var a = anemometerById(12)
+
+      Should(a).not.be.equal(null)
+
+      assertPromise(a)
+
+      a.then(function(anemometer) {
+        Should(anemometer).not.be.equal(null)
+      })
+    }))
+  })
 })
